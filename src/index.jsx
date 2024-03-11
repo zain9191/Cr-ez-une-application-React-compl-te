1a289484b4e261d4eff43286b56e5e8f7adbccd9
Zain <zain.frayha@gmail.com>
2024-03-11 15:51:06 +0100

    Transformez votre application en Single Page Application avec React Router

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/";
+import Survey from "./pages/Survey";
+import Header from "./components/Header";
+// On ajoute nos composants
+import ClientForm from "./components/ClientForm/index";
+import FreelanceForm from "./components/FreelanceForm/index";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey" element={<Survey />}>
+          {/* Nous imbriquons nos composants dans survey */}
+          <Route path="client" element={<ClientForm />} />
+          <Route path="freelance" element={<FreelanceForm />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,5 @@
 function Home() {
-  return <div>Page d'accueil 🏡
-
-
-
-    
-  </div>
+  return <div>Page d'accueil 🏡</div>;
 }
 
-export default Home 
+export default Home;
