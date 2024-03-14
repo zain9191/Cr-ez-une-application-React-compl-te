6989585ef731206f13c4e821716a0e7f4a3f5bee
Zain <zain.frayha@gmail.com>
2024-03-14 18:20:54 +0100

    Allez plus loin avec les hooks

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
 import Card from "../../components/Card";
 import styled from "styled-components";
 import colors from "../../utils/style/colors";
 import { Loader } from "../../utils/style/Atoms";
+import { useFetch, useTheme } from "../../utils/hooks";
 
 const CardsContainer = styled.div`
   display: grid;
@@ -15,9 +15,9 @@ const CardsContainer = styled.div`
 
 const PageTitle = styled.h1`
   font-size: 30px;
-  color: black;
   text-align: center;
   padding-bottom: 30px;
+  color: ${({ theme }) => (theme === "light" ? "#000000" : "#ffffff")};
 `;
 
 const PageSubtitle = styled.h2`
@@ -26,33 +26,24 @@ const PageSubtitle = styled.h2`
   font-weight: 300;
   text-align: center;
   padding-bottom: 30px;
+  color: ${({ theme }) => (theme === "light" ? "#000000" : "#ffffff")};
 `;
+
 const LoaderWrapper = styled.div`
   display: flex;
   justify-content: center;
 `;
 
 function Freelances() {
-  const [isDataLoading, setDataLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [freelancersList, setFreelancesList] = useState([]);
+  const { theme } = useTheme();
+  const { data, isLoading, error } = useFetch(
+    `http://localhost:8000/freelances`
+  );
 
-  useEffect(() => {
-    async function fetchFreelances() {
-      setDataLoading(true);
-      try {
-        const response = await fetch(`http://localhost:8000/freelances`);
-        const { freelancersList } = await response.json();
-        setFreelancesList(freelancersList);
-      } catch (err) {
-        console.log(err);
-        setError(true);
-      } finally {
-        setDataLoading(false);
-      }
-    }
-    fetchFreelances();
-  }, []);
+  // Ici le "?" permet de s'assurer que data existe bien.
+  // Vous pouvez en apprendre davantage sur cette notation ici :
+  // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+  const freelancersList = data?.freelancersList;
 
   if (error) {
     return <span>Oups il y a eu un problème</span>;
@@ -60,13 +51,13 @@ function Freelances() {
 
   return (
     <div>
-      <PageTitle>Trouvez votre prestataire</PageTitle>
-      <PageSubtitle>
+      <PageTitle theme={theme}>Trouvez votre prestataire</PageTitle>
+      <PageSubtitle theme={theme}>
         Chez Shiny nous réunissons les meilleurs profils pour vous.
       </PageSubtitle>
-      {isDataLoading ? (
+      {isLoading ? (
         <LoaderWrapper>
-          <Loader />
+          <Loader theme={theme} />
         </LoaderWrapper>
       ) : (
         <CardsContainer>
