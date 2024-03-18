e96592a2b654297b4efb1fd2b699359050e7c0b0
Zain <zain.frayha@gmail.com>
2024-03-18 20:10:37 +0100

    avec, Découvrez la base des tests dans React avec Jest

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Survey from "./pages/Survey";
 import Results from "./pages/Results";
